f4db8b263e5091d83bda32f23440649c423b2509
Lara Maia <dev@lara.click>
2018-12-14 17:26:58 -0200

    bump version

--- src/doc/conf.py
+++ src/doc/conf.py
@@ -28,7 +28,7 @@ author = 'Lara Maia'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.11'
+release = '0.11.1'
 
 
 # -- General configuration ---------------------------------------------------
