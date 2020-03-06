eac02f37850e4ec46cbecbe56ffcdaf44c091a44
Lara Maia <dev@lara.click>
2020-03-05 21:47:47 -0300

    bump version

--- setup.py
+++ setup.py
@@ -135,7 +135,7 @@ class InstallWithoutBuild(install):
 
 setup(
     name='stlib',
-    version='0.12.1',
+    version='0.12.2',
     description="Async library that provides features related to Steam client and compatible stuffs",
     author='Lara Maia',
     author_email='dev@lara.click',
