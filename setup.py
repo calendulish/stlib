b56b028ea1ffa37258cc91d7b76487194c7148a2
ShyPixie <dev@lara.monster>
2022-03-10 07:01:54 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ classifiers = [
 
 setup(
     name='stlib',
-    version='0.13.6',
+    version='0.14',
     description="Async library that provides features related to Steam client and compatible stuffs",
     author='Lara Maia',
     author_email='dev@lara.monster',
