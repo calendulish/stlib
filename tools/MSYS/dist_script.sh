22feb390b6464a26ce5642cda245b2e176ac04e4
ShyPixie <dev@lara.monster>
2022-04-28 16:51:32 -0300

    migrate auto build to GA

---
+++ tools/MSYS/dist_script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Lara Maia <dev@lara.monster> <YEAR>
+#
+# The <program> is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# The <program> is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see http://www.gnu.org/licenses/.
+#
+
+source "$(dirname "$0")/common.sh"
+
+check_msys
+
+#clean
+rm -rfv build dist
+
+# download steamworks
+pushd src/steam_api/steamworks_sdk || exit 1
+curl -o steamworks-sdk.zip -L https://github.com/ShyPixie/Overlays/blob/master/dev-util/steamworks-sdk/files/steamworks_sdk_151.zip?raw=true || exit 1
+unzip -o steamworks-sdk.zip || exit 1
+popd || exit 1
+
+# build project
+./setup.py -v build || exit 1
+pushd build || exit 1
+mv "exe.mingw_x86_64-$PYTHON_VERSION" "$RELEASE_NAME" || exit 1
+
+# zip release
+tar -vvcf "$RELEASE_NAME.zip" "$RELEASE_NAME" || exit 1
+popd || exit 1
+
+exit 0
---
+++ tools/Windows/update_dev_environment.cmd
@@ -0,0 +1,101 @@
+@echo off
+call %~dp0common.cmd
+::
+:: Lara Maia <dev@lara.monster> <YEAR>
+::
+:: The <program> is free software: you can redistribute it and/or
+:: modify it under the terms of the GNU General Public License as
+:: published by the Free Software Foundation, either version 3 of
+:: the License, or (at your option) any later version.
+::
+:: The <program> is distributed in the hope that it will be useful,
+:: but WITHOUT ANY WARRANTY; without even the implied warranty of
+:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+:: See the GNU General Public License for more details.
+::
+:: You should have received a copy of the GNU General Public License
+:: along with this program. If not, see http://www.gnu.org/licenses/.
+::
+
+echo Checking current environment...
+set MSYS_LATEST=https://github.com/msys2/msys2-installer/releases/download/nightly-x86_64/msys2-base-x86_64-latest.sfx.exe
+
+setlocal enabledelayedexpansion
+if not exist msys64 (
+
+    if not exist msys2.exe (
+        echo Downloading msys2...
+        set system32=%comspec%
+        set system32=!system32:cmd.exe=!
+        set curl="!system32!curl.exe"
+        set certutil="!system32!certutil.exe"
+        !curl! -o msys2.exe -L !MSYS_LATEST! || !certutil! -urlcache -split -f !MSYS_LATEST! msys2.exe || exit 1
+    )
+
+    if not exist msys64 (
+        echo Extracting msys2...
+        msys2.exe -y || exit 1
+    )
+
+)
+endlocal
+
+echo Fixing GPG
+:: gpg is unable to work when msys2 is running from a WSL path
+call :shell sed 's/SigLevel    = Required/SigLevel    = Never/' -i /etc/pacman.conf
+echo Installing core packages
+call :shell pacman -Syyuu --noconfirm
+echo Running normal system update
+call :shell pacman -Syu --noconfirm
+
+echo Installing Python
+call :install python
+call :install pip
+:: Reset python version
+call %~dp0common.cmd
+
+echo Updating setuptools to latest
+call :install setuptools --ignore-installed
+
+echo Installing dev tools
+call :install git tar unzip
+call :install gcc make
+call :install mypy pylint pytest
+
+echo Installing freezing tools
+call :install cx-freeze pywin32
+
+echo Updating project dependencies
+call :shell python setup.py egg_info
+set requires="src\\%project_name%.egg-info\\requires.txt"
+
+if exist %requires% (
+    echo Installing project dependencies
+
+    for /f "usebackq EOL=[ delims=" %%i in (%requires%) do (
+        call :install %%i
+    )
+)
+
+echo Cleaning...
+del /f /q /s msys2.exe >nul 2>&1 || exit 1
+
+echo Done.
+exit 0
+
+:shell
+%BINPATH%\\sh.exe -c "%*" || exit 1
+goto :eof
+
+:install
+setlocal enabledelayedexpansion
+for %%i in (%*) do (
+    set packages=!packages! %ARCH%-%%i
+    set python_packages=!python_packages! %ARCH%-python-%%i
+)
+
+set pacman=pacman -S --needed --noconfirm --overwrite
+set pip=python -m pip install
+%BINPATH%\\sh.exe -c "%pacman% !python_packages! 2>&- || %pacman% !packages! 2>&- || %pacman% %* 2>&- || %pip% %*" || exit 1
+endlocal
+goto :eof
