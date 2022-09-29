6568b431edb6729c68f943484034461c89e16b51
ShyPixie <dev@lara.monster>
2022-09-29 03:35:09 -0300

    [tests] add webapi manual testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,6 +16,91 @@
 # along with this program. If not, see http://www.gnu.org/licenses/.
 #
 
+import asyncio
+import configparser
+from pathlib import Path
+
+import pytest
+import pytest_asyncio
+
+from stlib import login, webapi, universe, community
+
+config_file = Path(__file__).parent.resolve() / 'conftest.ini'
+parser = configparser.RawConfigParser()
+parser.read(config_file)
+
 
 def pytest_assertion_pass(item, lineno, orig, expl):
     print(f"{item}:{lineno} -> {orig} resolves to {expl}")
+
+
+@pytest_asyncio.fixture(scope='session')
+def event_loop():
+    try:
+        return asyncio.get_running_loop()
+    except RuntimeError:
+        return asyncio.new_event_loop()
+
+
+@pytest.fixture(scope='session')
+def steamid() -> universe.SteamId:
+    steamid_ = universe.generate_steamid(parser.get('Test', 'steamid'))
+    return steamid_
+
+@pytest.fixture(scope='session')
+def oauth_token() -> str:
+    oauth_token_ = parser.get('Test', 'oauth_token')
+    return oauth_token_
+
+
+# noinspection PyShadowingNames
+@pytest_asyncio.fixture(scope='session')
+async def do_login(steamid) -> None:
+    token = parser.get('Test', 'token')
+    token_secure = parser.get('Test', 'token_secure')
+    shared_secret = parser.get('Test', 'shared_secret')
+
+    try:
+        login_session = await login.Login.new_session(0)
+    except IndexError:
+        login_session = login.Login.get_session(0)
+
+    login_session.restore_login(steamid, token, token_secure)
+
+    if not await login_session.is_logged_in(steamid):
+        login_session.username = parser.get('Test', 'account_name')
+        login_session.password = parser.get('Test', 'password_raw')
+
+        login_data = await login_session.do_login(shared_secret, mobile_login=True)
+        parser.set('Test', 'token', login_data.oauth['wgtoken'])
+        parser.set('Test', 'token_secure', login_data.oauth['wgtoken_secure'])
+        parser.set('Test', 'oauth_token', login_data.oauth['oauth_token'])
+
+        with open(config_file, 'w', encoding="utf8") as config_file_object:
+            parser.write(config_file_object)
+
+    return None
+
+
+# noinspection PyShadowingNames, PyUnusedLocal
+@pytest_asyncio.fixture(scope='module')
+async def community_session(do_login) -> community.Community:
+    try:
+        community_session_ = await community.Community.new_session(0)
+    except IndexError:
+        community_session_ = community.Community.get_session(0)
+
+    return community_session_
+
+
+# noinspection PyShadowingNames, PyUnusedLocal
+@pytest_asyncio.fixture(scope='module')
+async def webapi_session(do_login, community_session) -> webapi.SteamWebAPI:
+    api_key = await community_session.get_api_key()
+
+    try:
+        webapi_session_ = await webapi.SteamWebAPI.new_session(0, api_key=api_key)
+    except IndexError:
+        webapi_session_ = webapi.SteamWebAPI.get_session(0)
+
+    return webapi_session_
--- tests/webapi/test_webapi.py
+++ tests/webapi/test_webapi.py
@@ -15,10 +15,55 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see http://www.gnu.org/licenses/.
 #
+from stlib import universe, webapi, login
 
-from stlib import webapi
+from tests import requires_manual_testing, debug
 
 
-class TestWebApi:
+@requires_manual_testing
+async def test_server_time(webapi_session) -> None:
+    server_time = await webapi_session.get_server_time()
+    assert isinstance(server_time, int)
+    assert len(str(server_time)) == 10
+
+
+@requires_manual_testing
+async def test_get_custom_profile_url(webapi_session, steamid) -> None:
+    profile_url = await webapi_session.get_custom_profile_url(steamid)
+    assert isinstance(profile_url, str)
+    assert 'https://steamcommunity.com/id/' in profile_url
+    debug(profile_url, wait_for=0)
+
+
+@requires_manual_testing
+async def test_get_steamid(webapi_session, steamid) -> None:
+    profile_url = await webapi_session.get_custom_profile_url(steamid)
+    steamid_2 = await webapi_session.get_steamid(profile_url)
+    assert isinstance(steamid, universe.SteamId)
+    assert steamid == steamid_2
+
+
+@requires_manual_testing
+async def test_get_personaname(webapi_session, steamid) -> None:
+    persona = await webapi_session.get_personaname(steamid)
+    isinstance(persona, str)
+    debug(persona, wait_for=0)
+
+
+@requires_manual_testing
+async def test_get_owned_games(webapi_session, steamid) -> None:
+    owned_games = await webapi_session.get_owned_games(steamid)
+    assert isinstance(owned_games, list)
+    assert all(isinstance(game, webapi.Game) for game in owned_games)
+
+    owned_games_filtered = await webapi_session.get_owned_games(steamid, appids_filter=[220])
+    assert isinstance(owned_games_filtered, list)
+    assert all(isinstance(game, webapi.Game) for game in owned_games_filtered)
+    debug(str(owned_games_filtered[0]), wait_for=0)
+
+
+@requires_manual_testing
+async def test_new_authenticator(webapi_session, steamid, oauth_token) -> None:
+    login_data = webapi_session.new_authenticator(steamid, oauth_token)
+    isinstance(login_data, login.LoginData)
     # TODO
-    pass
