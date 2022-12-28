0565ac2114b6ba54efbf889e39fc177a47751fae
ShyPixie <dev@lara.monster>
2022-12-28 20:07:52 -0300

    Update community tests

---
+++ tests/community/test_community.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#
+# Lara Maia <dev@lara.monster> 2015 ~ 2022
+#
+# The stlib is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# The stlib is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see http://www.gnu.org/licenses/.
+#
+
+from tests import requires_manual_testing, debug
+
+
+@requires_manual_testing
+async def test_get_last_played_game(community_session, steamid) -> None:
+    last_played_game = await community_session.get_last_played_game(steamid)
+    assert isinstance(last_played_game, int)
+    debug(str(last_played_game), wait_for=0)
