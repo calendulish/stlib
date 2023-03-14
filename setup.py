6826b49bf40b2cc2deab51313550f08cb360acd1
ShyPixie <dev@lara.monster>
2023-03-14 20:14:55 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -99,7 +99,7 @@ except FileNotFoundError:
 
 setup(
     name='stlib',
-    version='1.1.1',
+    version='1.2',
     description="Async library that provides features related to Steam client and compatible stuffs",
     long_description=long_description,
     long_description_content_type='text/markdown',
