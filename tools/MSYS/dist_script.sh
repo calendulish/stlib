6d715969dcb8eb8b490557ec9a8aa8a8bae4ec78
Lara Maia <dev@lara.monster>
2024-06-08 01:05:58 -0300

    [CI] fix msys build

--- tools/MSYS/dist_script.sh
+++ tools/MSYS/dist_script.sh
@@ -29,7 +29,7 @@ popd || exit 1
 # build project
 python -m build --sdist --wheel --no-isolation || exit 1
 pushd build || exit 1
-mv "lib.mingw_x86_64-$PYTHON_VERSION" "$RELEASE_NAME" || exit 1
+mv "lib.mingw_x86_64-cpython-${PYTHON_VERSION/./}" "$RELEASE_NAME" || exit 1
 
 # zip release
 tar -vvcf "$RELEASE_NAME.zip" "$RELEASE_NAME" || exit 1
